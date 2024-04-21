824c06ab5c1a4cb28833aea85d84d97dfba1f0e9
shiya <2935726891@qq.com>
2024-04-21 22:43:16 +0800

    ref属性

---
+++ vue_example/02_src_ref属性/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1 ref="title">hhhhhhhhh</h1>
+    <button @click="show" ref="btn">点我输出上面的dom</button>
+    <School ref="sch"></School>
+    <School />
+    <School />
+  </div>
+</template>
+
+<script>
+import School from "./components/School";
+export default {
+  name: "App",
+  components: {
+    School,
+  },
+  data() {
+    return {
+      msg: "你好啊!",
+    };
+  },
+  methods: {
+    show() {
+      console.log(this.$refs.title); // 真实的dom元素
+      console.log(this.$refs.btn); // 真实的dom元素
+      console.log(this.$refs.sch); // school组件的实例对象 vc
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- vue_example/02_src_ref属性/main.js
+++ vue_example/02_src_ref属性/main.js
@@ -2,11 +2,15 @@
 import Vue from 'vue' // 残缺版的vue
 
 // 引入App组件，它是所有组件的父组件
-import App from './App.vue'
+import App from './App'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
+
 
